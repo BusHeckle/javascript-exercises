d44abd41b34a91dd979d6034f3f9175ff9b00b57
BusHeckle <167415320+BusHeckle@users.noreply.github.com>
2024-05-06 16:22:51 +0100

    Solution to 05_SUMALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
 
-};
+    let total = 0; //- create a variable to hold the final sum
 
+    //check if start and end numbers are positive and integers first:
+
+    if (startNumber < 0 || endNumber < 0 || !Number.isInteger(startNumber) || !Number.isInteger(endNumber) ){
+        return "ERROR";
+    } else {
+        
+        function sumAllNumbersBetween(startNumberSum, endNumberSum){
+            //loop through the given numbers, on each iteration adding the number to the total:
+            for (let i=startNumberSum; i <= endNumberSum; i++){
+                total += startNumberSum;
+                startNumberSum ++;
+           }
+           return total;
+        }
+        //Check if startNumber is smaller than endNumber...
+        if (startNumber < endNumber) {
+            //... if so, sumAllNumbersBetween
+            sumAllNumbersBetween(startNumber, endNumber);
+            
+        } else {
+
+            //Otherwise, startNumber is bigger than endNumber, so reverse numbers:
+            sumAllNumbersBetween(endNumber, startNumber);
+        }
+        //finally, return the total:
+        return total;
+    }
+
+}
 // Do not edit below this line
 module.exports = sumAll;
