25b2d878c888aedc3b2adda57f966f7a0e572daf
BusHeckle <167415320+BusHeckle@users.noreply.github.com>
2024-05-06 15:33:50 +0100

    Solution to 03_REVERSESTRING

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    const splitPhrase = phrase.split(""); //create an array containing input phrase e.g. "hello" > ["h","e","l","l","o"] 
+    const splitPhraseReversed = splitPhrase.reverse(); //reverse splitPhrase e.g. ["o","l","l", "e","h"]
+    const joinSplitPhrase = splitPhraseReversed.join(""); // join splitPhraseReversed into string e.g. "olleh"
+    return joinSplitPhrase; //return joinSplitPhrase as final output
 };
 
 // Do not edit below this line
