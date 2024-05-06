82620d69cda984c825ea3a43cba3a7b57f38d2c2
BusHeckle <167415320+BusHeckle@users.noreply.github.com>
2024-05-06 15:13:55 +0100

    Solution to 02_REPEATSTRING

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    //create a variable of string that is empty
+    let string = "";
+    //create a loop that repeats as many times as the variable "times"
+    for (let i = 0; i < times; i++) {
+        // add the variable word to the string each time
+        string += word; 
+    }
+    return string;
 };
 
 // Do not edit below this line
